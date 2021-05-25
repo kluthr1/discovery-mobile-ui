6ad9f29ba27a6c6e5baed3e5eae0e584c5d1e161
erwin foxtree <foxtree@users.noreply.github.com>
2021-05-25 13:23:03 -0400

    Use AuthSession + PKCE to log in to Epic sandbox

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,47 +1,39 @@
-import React, { useState } from 'react';
-import { useSetRecoilState } from 'recoil';
+import React, { useState, useEffect } from 'react';
 import {
-  Alert,
   StyleSheet,
-  Text,
   View,
-  TouchableOpacity,
 } from 'react-native';
+import FhirKitClient from 'fhir-kit-client';
+import Constants from 'expo-constants'; // eslint-disable-line import/no-extraneous-dependencies
 
-import { authenticationState } from '../../recoil';
 import Colors from '../../constants/Colors';
-import {
-  authAsync, buildFhirIssUrl,
-} from '../../resources/fhirAuth';
-
-import PatientPicker, { DEFAULT_PATIENT_ID } from './PatientPicker';
-import SkipLoginButton from './SkipLoginButton';
 import LoadingIndicator from '../LoadingIndicator';
 
-const Login = () => {
-  const setAuthentication = useSetRecoilState(authenticationState);
-  const [loading, setLoading] = useState(false);
-  const [mockPatientId, setPatientId] = useState(DEFAULT_PATIENT_ID);
-  const fhirIss = buildFhirIssUrl(mockPatientId);
+import PatientPicker, { DEFAULT_PATIENT_ID } from './PatientPicker';
+import LoginButton from './LoginButton';
+import SkipLoginButton from './SkipLoginButton';
 
-  const handleLogin = async () => {
-    setLoading(true);
-    try {
-      const authResponse = await authAsync(fhirIss);
-      if (authResponse) {
-        setLoading(false);
-        setAuthentication({
-          baseUrl: fhirIss,
-          authResult: authResponse,
-        });
-      }
-    } catch (error) {
-      console.error('AppAuth Error:', error); // eslint-disable-line no-console
-      Alert.alert('Login Error', 'Must login to use Discovery', ['ok']);
-      // enable patient-picker and login buttons to render:
-      setLoading(false);
-    }
-  };
+// https://open.epic.com/MyApps/Endpoints
+const { BASE_URL } = Constants.manifest.extra;
+
+const Login = () => {
+  const [loading, setLoading] = useState(false);
+  const [authorizeUrl, setAuthorizeUrl] = useState(false);
+  const [tokenUrl, setTokenUrl] = useState(false);
+  const [mockPatientId, setPatientId] = useState(DEFAULT_PATIENT_ID);
+
+  useEffect(() => {
+    const fhirClient = new FhirKitClient({ baseUrl: BASE_URL });
+    fhirClient.smartAuthMetadata().then((fkcResult) => {
+      const { authorizeUrl: auth, tokenUrl: tok } = fkcResult;
+      setAuthorizeUrl(auth);
+      setTokenUrl(tok);
+    });
+  }, []);
+
+  if (!authorizeUrl || !tokenUrl) {
+    return <LoadingIndicator />;
+  }
 
   return (
     <View style={styles.body}>
@@ -54,12 +46,12 @@ const Login = () => {
             setPatientId={setPatientId}
           />
           <SkipLoginButton />
-          <TouchableOpacity
-            style={styles.login}
-            onPress={handleLogin}
-          >
-            <Text style={styles.loginText}>Login</Text>
-          </TouchableOpacity>
+          <LoginButton
+            setLoading={setLoading}
+            baseUrl={BASE_URL}
+            authorizeUrl={authorizeUrl.toString()}
+            tokenUrl={tokenUrl.toString()}
+          />
         </>
       )}
     </View>
--- src/redux/middleware/fhir-client.js
+++ src/redux/middleware/fhir-client.js
@@ -1,26 +1,46 @@
 import Client from 'fhir-kit-client';
 // import { Alert } from 'react-native';
 
+const { Buffer } = require('buffer');
+
 const RESOURCE_TYPES = [
-  'Patient',
-  'ExplanationOfBenefit',
-  'Claim',
-  'Condition',
+  // 'Patient',
+  // 'ExplanationOfBenefit',
+  // 'Claim',
+  // 'Condition', // "A required element is missing."
   'Encounter',
   'Immunization',
   'MedicationRequest',
-  'CarePlan',
+  // 'CarePlan',
   'DiagnosticReport',
   'Procedure',
-  'Observation',
+  // 'Observation',
 ];
 
-export default class FhirClient {
-  constructor() {
-    this.client = null;
+// JSON Web Token (JWT) Profile for OAuth 2.0 Client Authentication and Authorization Grants
+//     https://datatracker.ietf.org/doc/html/rfc7523
+// JSON Web Token Best Current Practices
+//     https://datatracker.ietf.org/doc/html/rfc8725
+export const decodeIdToken = (idToken) => idToken.split('.').map((data, index) => {
+  // return `https://launch.smarthealthit.org/v/r4/sim/${Buffer.from(issDataString).toString('base64')}/fhir`;
+  // const buffer = new Buffer(data, 'base64');
+  const buffer = Buffer.from(data, 'base64');
+  const segment = buffer.toString('ascii');
+  if (index < 2) {
+    try {
+      return JSON.parse(segment.toString());
+    } catch (e) {
+      console.error('decodeIdToken ERROR: ', e); // eslint-disable-line no-console
+    }
   }
+  return segment;
+});
 
-  initialize(baseUrl, accessToken) {
+export default class FhirClient {
+  constructor(baseUrl, accessToken, idToken) {
+    this.decodedIdToken = decodeIdToken(idToken);
+    this.patientId = this.decodedIdToken[1].sub;
+
     this.client = new Client({
       baseUrl,
       customHeaders: {
@@ -29,21 +49,16 @@ export default class FhirClient {
     });
   }
 
-  async queryPatient(patientId) {
-    return this.client.batch({
-      body: {
-        resourceType: 'Bundle',
-        type: 'batch',
-        entry: RESOURCE_TYPES.map((type) => (
-          {
-            request: {
-              method: 'GET',
-              url: (type === 'Patient') ? `${type}/${patientId}` : `${type}?patient=${patientId}`,
-            },
-          }
-        )),
+  queryPatient() {
+    const { patientId } = this;
+    const requests = [() => this.client.request(`Patient/${patientId}`)];
+
+    return requests.concat(RESOURCE_TYPES.map((resourceType) => () => (this.client.search({
+      resourceType,
+      searchParams: {
+        patient: patientId,
       },
-    });
+    }))));
   }
 
   async request(url) {
