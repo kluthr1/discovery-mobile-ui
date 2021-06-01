1d36db760e2e9734612397af61fa8b16bec1f586
erwin foxtree <foxtree@users.noreply.github.com>
2021-06-01 16:22:39 -0400

    app.config.js: export and lint

--- app.config.js
+++ app.config.js
@@ -1,33 +1,33 @@
-{
-  "expo": {
-    "name": "discovery-mobile-ui",
-    "slug": "discovery-mobile-ui",
-    "owner": "sync-for-science",
-    "version": "1.0.0",
-    "orientation": "portrait",
-    "icon": "./assets/icon.png",
-    "splash": {
-      "image": "./assets/splash.png",
-      "resizeMode": "contain",
-      "backgroundColor": "#ffffff"
+export default {
+  expo: {
+    name: 'discovery-mobile-ui',
+    slug: 'discovery-mobile-ui',
+    owner: 'sync-for-science',
+    version: '1.0.0',
+    orientation: 'portrait',
+    icon: './assets/icon.png',
+    splash: {
+      image: './assets/splash.png',
+      resizeMode: 'contain',
+      backgroundColor: '#ffffff',
     },
-    "updates": {
-      "fallbackToCacheTimeout": 0
+    updates: {
+      fallbackToCacheTimeout: 0,
     },
-    "assetBundlePatterns": [
-      "**/*"
+    assetBundlePatterns: [
+      '**/*',
     ],
-    "ios": {
-      "supportsTablet": true
+    ios: {
+      supportsTablet: true,
     },
-    "android": {
-      "adaptiveIcon": {
-        "foregroundImage": "./assets/adaptive-icon.png",
-        "backgroundColor": "#FFFFFF"
-      }
+    android: {
+      adaptiveIcon: {
+        foregroundImage: './assets/adaptive-icon.png',
+        backgroundColor: '#FFFFFF',
+      },
     },
-    "web": {
-      "favicon": "./assets/favicon.png"
-    }
-  }
-}
+    web: {
+      favicon: './assets/favicon.png',
+    },
+  },
+};
